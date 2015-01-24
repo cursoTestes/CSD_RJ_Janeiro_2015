222d8a1b932499151b2cc4d4ad4cc25a879c50c2
cfc <cfc@ufrj.br>
2015-01-24 00:00:59 -0200

    mostrando a margem de erro

--- SistemaVendas/testes/br/com/k21/TestCalculadoraComissao.java
+++ SistemaVendas/testes/br/com/k21/TestCalculadoraComissao.java
@@ -32,7 +32,7 @@ public class TestCalculadoraComissao {
 		
 		comissaoObtida = calculadoraComissao.calcular(venda);
 		
-		assertEquals(comissaoEsperada, comissaoObtida, 0.000001);
+		assertEquals(comissaoEsperada, comissaoObtida, 0.001);
 	}
 	
 	@Test
