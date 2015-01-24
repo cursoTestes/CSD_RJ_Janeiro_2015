9f6123d6ab080c8f2d43facd93821544e94f370c
cfc <cfc@ufrj.br>
2015-01-24 00:18:12 -0200

    resolvendo do jeito certo agora

--- SistemaVendas/src/br/com/k21/CalculadoraComissao.java
+++ SistemaVendas/src/br/com/k21/CalculadoraComissao.java
@@ -3,7 +3,14 @@ package br.com.k21;
 public class CalculadoraComissao {
 
 	public double calcular(double venda) {
-		return venda*0.05;
 		
+		double comissao = venda *0.05;
+		comissao = arredondaParaEmpresaNaoLevarPrejuizo(comissao);
+		
+		return comissao;
+	}
+
+	private double arredondaParaEmpresaNaoLevarPrejuizo(double comissao) {
+		return Math.floor(comissao*100)/100;
 	}
 }
